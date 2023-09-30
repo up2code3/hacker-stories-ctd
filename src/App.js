f4c61d60b5650e5a098eee2ef1078bd16012fec1
Tristan White <113216091+up2code3@users.noreply.github.com>
2023-09-30 18:26:35 -0400

    Embedded JS in JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import * as React from 'react';
 
+function getTitle(title){
+  return title;
+}
+
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {getTitle("world")}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+      
     </div>
   );
 }
