1b376d6f54a615da805e36277de9951ea4880c42
Tristan White <113216091+up2code3@users.noreply.github.com>
2023-10-01 15:28:49 -0400

    used item.key to extrat array items

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import * as React from 'react';
 
-function getTitle(title){
-  return title;
-}
+const list = [
+  {
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
+  {
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+  },
+];
 
+
+// {function listTHEitems (item) {return <li>{item.title}</li>}}
 
 function App() {
   return (
     <div>
-      <h1>Hello {getTitle("world")}</h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
       
+      <hr />
+      
+      <ul> 
+        {list.map(function(item) {
+          return (
+              <li key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author + " "}</span>
+                <span>{item.num_comments + " "}</span>
+                <span>{item.points + " "}</span>
+              </li>
+            );
+        })}
+      </ul>
+
     </div>
   );
 }
