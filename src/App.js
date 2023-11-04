775885af8d87309123e26272f8ff1f25957abcfa
Tristan White <113216091+up2code3@users.noreply.github.com>
2023-11-04 18:17:30 -0400

    back to item

--- src/App.js
+++ src/App.js
@@ -12,9 +12,11 @@ const List = (props) =>
 const Item = ({ item }) => 
   
     <li key={item.objectID}>
+      
       <span>
         <a href={item.url}>{item.title}</a>
       </span>
+      
       <span>{item.author + " "}</span>
       <span>{item.num_comments + " "}</span>
       <span>{item.points + " "}</span>
